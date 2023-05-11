b088e23ab807e6ec991614cd711555f81fcfe4cc
ãyoobeommo <kiekangmin@naver.com>
2023-05-11 11:00:35 +0900

    회원가입 에러 예외처리 수정

--- src/main/java/com/example/miniproject14/service/UserService.java
+++ src/main/java/com/example/miniproject14/service/UserService.java
@@ -27,28 +27,34 @@ public class UserService {
 
     @Transactional
     public GeneralResponseDto signup(SignupRequestDto signupRequestDto) {
-        String username = signupRequestDto.getUsername();
-        String nickname = signupRequestDto.getNickname();
-        String password = passwordEncoder.encode(signupRequestDto.getPassword());
+        try {
+            String username = signupRequestDto.getUsername();
+            String nickname = signupRequestDto.getNickname();
+            String password = passwordEncoder.encode(signupRequestDto.getPassword());
 
-        // 회원 중복 확인
-        Optional<User> found = userRepository.findByUsername(username);
-        if (found.isPresent()) {
-            throw new IllegalArgumentException("중복된 ID 입니다.");
-        }
-
-        // 사용자 ROLE 확인
-        UserRoleEnum role = UserRoleEnum.USER;
-        if (signupRequestDto.isAdmin()) {
-            if (!signupRequestDto.getAdminToken().equals(ADMIN_TOKEN)) {
-                throw new IllegalArgumentException("관리자 암호가 틀려 등록이 불가능합니다.");
+            // 회원 중복 확인
+            Optional<User> found = userRepository.findByUsername(username);
+            if (found.isPresent()) {
+                throw new IllegalArgumentException("중복된 ID 입니다.");
             }
-            role = UserRoleEnum.ADMIN;
-        }
 
-        User user = new User(username, nickname, password, role);
-        userRepository.save(user);
-        return new StatusResponseDto("회원 가입 완료!", HttpStatus.OK);                  // DB에 정상적으로 저장 되었을 경우 결과 리턴
+            // 사용자 ROLE 확인
+            UserRoleEnum role = UserRoleEnum.USER;
+            if (signupRequestDto.isAdmin()) {
+                if (!signupRequestDto.getAdminToken().equals(ADMIN_TOKEN)) {
+                    throw new IllegalArgumentException("관리자 암호가 틀려 등록이 불가능합니다.");
+                }
+                role = UserRoleEnum.ADMIN;
+            }
+
+            User user = new User(username, nickname, password, role);
+            userRepository.save(user);
+            return new StatusResponseDto("회원 가입 완료!", HttpStatus.OK); // DB에 정상적으로 저장 되었을 경우 결과 리턴
+        } catch (IllegalArgumentException e) {
+            return new StatusResponseDto(e.getMessage(), HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new StatusResponseDto("회원 가입 중 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @Transactional(readOnly = true)
